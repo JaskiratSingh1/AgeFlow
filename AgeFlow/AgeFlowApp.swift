7eb2ea1476629d382c05ae0699c007575a63a3cd
Jaskirat Singh <jaskirat1999@gmail.com>
2025-01-07 08:02:28 -0700

    Initial Commit

---
+++ AgeFlow/AgeFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgeFlowApp.swift
+//  AgeFlow
+//
+//  Created by Jaskirat Singh on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct AgeFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
